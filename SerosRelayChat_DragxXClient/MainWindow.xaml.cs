5a48c45e2a93ef40fc638b816adf6b8169f9a382
DragxX <daniel.schellhaas@gmail.com>
2013-03-24 01:08:39 +0100

    Client: added server connect

--- SerosRelayChat_DragxXClient/MainWindow.xaml.cs
+++ SerosRelayChat_DragxXClient/MainWindow.xaml.cs
@@ -22,7 +22,9 @@ namespace SerosRelayChat_DragxXClient
     /// </summary>
     public partial class MainWindow : Window
     {
-        Socket clientSocket;
+        public Socket clientSocket;
+        public string userName;
+        public byte[] byteData = new byte[1024];
 
         public MainWindow()
         {
@@ -33,13 +35,31 @@ namespace SerosRelayChat_DragxXClient
 
         private void startConnectionButton_Click(object sender, RoutedEventArgs e)
         {
-            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-            IPEndPoint ipEP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1234);
-            clientSocket.BeginConnect(ipEP, new AsyncCallback(onConnect), clientSocket);
+            connectionForm cF = new connectionForm(this);
+            cF.Owner = this;
+            cF.ShowDialog();
 
-            connectionStateLabel.Content = "Verbinde zu " + ipEP.Address + " ...";
+            chatLog.Text = "";
         }
-        
+
+        public void startConnect(string ip, int port, string username)
+        {
+            try
+            {
+                clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                IPEndPoint ipEP = new IPEndPoint(IPAddress.Parse(ip), port);
+                clientSocket.BeginConnect(ipEP, new AsyncCallback(onConnect), clientSocket);
+
+                connectionStateLabel.Content = "Verbinde zu " + ipEP.Address + " ...";
+
+                this.userName = username;
+            }
+            catch
+            {
+                MessageBox.Show("Fehlerhafte angaben");
+            }
+        }
+
         /// <summary>
         /// Ermöglicht das ändern des Status Labels
         /// </summary>
@@ -50,6 +70,15 @@ namespace SerosRelayChat_DragxXClient
         }
 
         /// <summary>
+        /// Neue Chatlog Meldung
+        /// </summary>
+        /// <param name="text">Zu ändender Text</param>
+        public void addChatLogMessage(string text)
+        {
+            chatLog.Text += text + "\n";
+        }
+
+        /// <summary>
         /// Methode zur Verbindungsherstellung
         /// </summary>
         /// <param name="aR">AsyncResult der Verbindung</param>
@@ -57,11 +86,23 @@ namespace SerosRelayChat_DragxXClient
         {
             Socket server = aR.AsyncState as Socket;
 
+            byte[] message = new byte[1024];
+            
+            Protocol sendingMsg = new Protocol();
+
             try
             {
                 if (server.Connected)
-                {
-                    this.Dispatcher.Invoke((Action) delegate() { setConnectionLabel("Verbindung hergestellt"); });
+                {                   
+                    sendingMsg.vonUser = userName;
+                    sendingMsg.Command = "LOGIN";
+                    sendingMsg.Arg = new String[] { "" };
+                    message = sendingMsg.ToByte();
+                    server.BeginSend(message, 0, message.Length, SocketFlags.None, new AsyncCallback(OnSend), server);
+
+                    clientSocket.BeginReceive(byteData, 0, byteData.Length, SocketFlags.None, new AsyncCallback(onRecieve), clientSocket);
+
+                    this.Dispatcher.Invoke((Action)delegate() { setConnectionLabel("Verbindung hergestellt"); });
                 }
                 else
                 {
@@ -69,6 +110,72 @@ namespace SerosRelayChat_DragxXClient
                 }
             }
             catch { }
+        }
+
+        private void OnSend(IAsyncResult ar)
+        {
+            try
+            {
+                Socket server = (Socket)ar.AsyncState;
+                server.EndSend(ar);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                //writeError(ex.ToString());
+            }
+        }
+
+        private void onRecieve(IAsyncResult ar)
+        {
+            try
+            {
+                Socket clientSocket = (Socket)ar.AsyncState;
+                clientSocket.EndReceive(ar);
+
+                Protocol recievedMsg = new Protocol(byteData);
+                this.Dispatcher.Invoke((Action)delegate() { addChatLogMessage(recievedMsg.vonUser + ": " + ConvertSringArrayToString(recievedMsg.Arg)); });
+
+                clientSocket.BeginReceive(byteData, 0, byteData.Length, SocketFlags.None, new AsyncCallback(onRecieve), clientSocket);
+            }
+            catch
+            {
+                this.Dispatcher.Invoke((Action)delegate() { addChatLogMessage("Verbindung verloren"); });
+                this.Dispatcher.Invoke((Action)delegate() { setConnectionLabel("Verbindung verloren"); });
+            }
+        }
+
+        private String ConvertSringArrayToString(String[] strArray)
+        {
+            StringBuilder StrBuilder = new StringBuilder();
+            foreach (String str in strArray)
+            {
+                StrBuilder.Append(str);
+                StrBuilder.Append(" ");
+            }
+            return StrBuilder.ToString();
+        }
+
+        private void sendMessageButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string[] splittedInput = chatMessageBox.Text.Split(' ');
+                byte[] message;
+
+                Protocol sendMessage = new Protocol();
+                sendMessage.vonUser = userName;
+                sendMessage.Command = splittedInput[0];
+                sendMessage.Arg = splittedInput.Skip(1).ToArray();
+
+                message = sendMessage.ToByte();
+
+                clientSocket.BeginReceive(message, 0, message.Length, SocketFlags.None, new AsyncCallback(onRecieve), clientSocket);
+            }
+            catch
+            {
+
+            }
         }
     }
 }
